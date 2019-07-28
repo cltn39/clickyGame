2d87f473a80d08bddc5ed7527470a40e1a73951e
gravitation <chisong@ucdavis.edu>
2019-07-28 01:06:01 -0700

    Updates

---
+++ precache-manifest.1a66fe5a6cb8c1a7195eb3d72e78bb08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb09e99d7096e347ff28e8055bc76a96",
+    "url": "/clickyGame/index.html"
+  },
+  {
+    "revision": "7c5497a9ccf619d08826",
+    "url": "/clickyGame/static/css/main.95ace9f3.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clickyGame/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "7c5497a9ccf619d08826",
+    "url": "/clickyGame/static/js/main.58096c7a.chunk.js"
+  },
+  {
+    "revision": "95f5ce002aae55a30c31",
+    "url": "/clickyGame/static/js/runtime~main.d5f62368.js"
+  }
+]);
